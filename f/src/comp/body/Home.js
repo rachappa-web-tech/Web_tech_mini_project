12781ca8e5fae980319675ced9146a439a614187
Rachappa <97609401+rachappa-web-tech@users.noreply.github.com>
2022-11-26 15:44:37 +0530

    70%

--- f/src/comp/body/Home.js
+++ f/src/comp/body/Home.js
@@ -3,8 +3,17 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-        <div className=''>
-            
+
+
+        <div className='container-fluid bg-primary '>
+<h1 className='text-white'>This is home page</h1>
+<h2 className='text-dark'>
+Students Reminder App
+</h2>
+<h6 className='text-white'>
+    Our site is constant Reminder app 
+    which send students constant Reminder by email and phone call
+</h6>
         </div>
     </div>
 
